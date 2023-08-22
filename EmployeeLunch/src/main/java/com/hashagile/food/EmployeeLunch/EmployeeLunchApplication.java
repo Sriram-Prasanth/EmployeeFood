6ddb66deef0616a20c8c629b6b155f3fa2689cd3
Sriram-Prasanth <sriramprasanth.palanisamy@hashagile.com>
2023-08-22 15:25:48 +0530

    Spring INittialized

---
+++ EmployeeLunch/src/main/java/com/hashagile/food/EmployeeLunch/EmployeeLunchApplication.java
@@ -0,0 +1,13 @@
+package com.hashagile.food.EmployeeLunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLunchApplication.class, args);
+	}
+
+}
---
+++ EmployeeLunch/src/test/java/com/hashagile/food/EmployeeLunch/EmployeeLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashagile.food.EmployeeLunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
